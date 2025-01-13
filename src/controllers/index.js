917b9b03db6f5534212f4cecc6636d9129fcfadb
Thea Win <mstheawin@gmail.com>
2025-01-13 10:09:24 +0400

    2 spacing format corrected

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,7 +1,7 @@
 class IndexController {
-    getIndex(req, res) {
-        res.send('Welcome to the Planner API');
-    }
+  getIndex(req, res) {
+    res.send('Welcome to the Planner API');
+  }
 }
 
 module.exports = IndexController;
--- src/models/index.js
+++ src/models/index.js
@@ -1,23 +1,23 @@
 class Planner {
-    constructor(data) {
-        this.data = data;
-    }
+  constructor(data) {
+    this.data = data;
+  }
 
-    save() {
-        // Logic to save planner data
-    }
+  save() {
+    // Logic to save planner data
+  }
 
-    find() {
-        // Logic to find planner data
-    }
+  find() {
+    // Logic to find planner data
+  }
 
-    update(id, newData) {
-        // Logic to update planner data
-    }
+  update(id, newData) {
+    // Logic to update planner data
+  }
 
-    delete(id) {
-        // Logic to delete planner data
-    }
+  delete(id) {
+    // Logic to delete planner data
+  }
 }
 
 module.exports = Planner;
