de7cee94a5270083118d78a06667a2b595bbd509
Thea Win <mstheawin@gmail.com>
2025-01-13 18:21:25 +0400

    Protect routes with JWT

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
+// filepath: src/routes/index.js
 const IndexController = require('../controllers/index');
 const CalendarController = require('../controllers/calendar');
 const TaskController = require('../controllers/task');
+const passport = require('passport');
 
 function setRoutes(app) {
   const indexController = new IndexController();
@@ -11,53 +13,75 @@ function setRoutes(app) {
 
   app.post(
     '/calendars',
+    passport.authenticate('jwt', { session: false }),
     calendarController.createCalendar.bind(calendarController)
   );
   app.get(
     '/calendars',
+    passport.authenticate('jwt', { session: false }),
     calendarController.getAllCalendars.bind(calendarController)
   );
   app.get(
     '/calendars/:calendarName',
+    passport.authenticate('jwt', { session: false }),
     calendarController.getCalendar.bind(calendarController)
   );
   app.put(
     '/calendars/:calendarName',
+    passport.authenticate('jwt', { session: false }),
     calendarController.editCalendar.bind(calendarController)
   );
   app.delete(
     '/calendars/:calendarName',
+    passport.authenticate('jwt', { session: false }),
     calendarController.deleteCalendar.bind(calendarController)
   );
 
-  app.post('/tasks', taskController.createTask.bind(taskController));
-  app.get('/tasks', taskController.getAllTasks.bind(taskController));
+  app.post(
+    '/tasks',
+    passport.authenticate('jwt', { session: false }),
+    taskController.createTask.bind(taskController)
+  );
+  app.get(
+    '/tasks',
+    passport.authenticate('jwt', { session: false }),
+    taskController.getAllTasks.bind(taskController)
+  );
   app.get(
     '/tasks/namesearch/:taskName',
+    passport.authenticate('jwt', { session: false }),
     taskController.getTaskByName.bind(taskController)
   );
   app.get(
     '/tasks/idsearch/:taskId',
+    passport.authenticate('jwt', { session: false }),
     taskController.getTaskById.bind(taskController)
   );
-  app.put('/tasks/:taskId', taskController.editTask.bind(taskController));
+  app.put(
+    '/tasks/:taskId',
+    passport.authenticate('jwt', { session: false }),
+    taskController.editTask.bind(taskController)
+  );
   app.get(
     '/tasks/:calendarName',
+    passport.authenticate('jwt', { session: false }),
     taskController.getTaskByCalendar.bind(taskController)
   );
   app.get(
     '/tasks/date/:dueDate',
+    passport.authenticate('jwt', { session: false }),
     taskController.getTaskByDate.bind(taskController)
-  );
-  app.get(
-    '/tasks/week/:dueDate',
-    taskController.getTaskByWeek.bind(taskController)
   );
   app.patch(
     '/tasks/:taskId',
+    passport.authenticate('jwt', { session: false }),
     taskController.toggleTaskCompletion.bind(taskController)
   );
-  app.delete('/tasks/:taskId', taskController.deleteTask.bind(taskController));
+  app.delete(
+    '/tasks/:taskId',
+    passport.authenticate('jwt', { session: false }),
+    taskController.deleteTask.bind(taskController)
+  );
 }
 
 module.exports = setRoutes;
