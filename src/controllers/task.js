9268b3014125bf90fa8b91971fde5fdeeaa35301
Thea Win <mstheawin@gmail.com>
2025-01-13 15:14:29 +0400

    endpoint for creating task

---
+++ src/controllers/task.js
@@ -0,0 +1,37 @@
+const uuid = require('uuid');
+const task = require('../models/task');
+
+class TaskController {
+  async createTask(req, res) {
+    const {
+      taskName,
+      taskDescription,
+      dueDate: reqDueDate,
+      calendarName,
+      priority,
+    } = req.body;
+    const dueDate = reqDueDate || new Date();
+
+    if (!taskName) {
+      return res.status(400).send('Task name is required');
+    }
+    if (!calendarName) {
+      return res.status(400).send('Calendar name is required');
+    }
+
+    try {
+      const newTask = await task.create({
+        taskName,
+        taskDescription,
+        dueDate,
+        calendarName,
+        priority,
+      });
+      res.status(201).send(newTask);
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+  }
+}
+
+module.exports = TaskController;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const IndexController = require('../controllers/index');
 const CalendarController = require('../controllers/calendar');
+const TaskController = require('../controllers/task');
 
 function setRoutes(app) {
   const indexController = new IndexController();
   const calendarController = new CalendarController();
+  const taskController = new TaskController();
 
   app.get('/', indexController.getIndex.bind(indexController));
 
@@ -27,6 +29,8 @@ function setRoutes(app) {
     '/calendars/:calendarName',
     calendarController.deleteCalendar.bind(calendarController)
   );
+
+  app.post('/tasks', taskController.createTask.bind(taskController));
 }
 
 module.exports = setRoutes;
