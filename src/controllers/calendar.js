6dd4acb7a5d5742fd2e5eb5cbb673505705c94fe
Thea Win <mstheawin@gmail.com>
2025-01-13 18:21:43 +0400

    update calendar creation to link it with user

--- src/controllers/calendar.js
+++ src/controllers/calendar.js
@@ -1,5 +1,6 @@
 const uuid = require('uuid');
 const calendar = require('../models/calendar');
+const user = require('../models/user');
 
 class CalendarController {
   async createCalendar(req, res) {
@@ -10,7 +11,17 @@ class CalendarController {
     }
 
     try {
-      const newCalendar = await calendar.create({ calendarName });
+      const newCalendar = await calendar.create({
+        calendarName,
+        user: req.user.username,
+      });
+
+      await user.findOneAndUpdate(
+        req.user.username,
+        { $push: { calendars: newCalendar._id } },
+        { new: true, useFindAndModify: false }
+      );
+
       res.status(201).send(newCalendar);
     } catch (error) {
       res.status(500).send(error.message);
